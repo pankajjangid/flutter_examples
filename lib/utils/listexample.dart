4d82af8704c29ec893a6ef2cc485c28a461cee90
Pankaj Jangid <pankaj0619@gmail.com>
2020-03-15 19:52:16 +0530

    Api calling added

---
+++ lib/utils/listexample.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+
+import 'package:demo2020/constants.dart';
+import 'package:demo2020/utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+class CustomList extends StatefulWidget {
+  @override
+  _CustomListState createState() => _CustomListState();
+}
+
+class _CustomListState extends State<CustomList> {
+  String api = "https://swapi.co/api/people";
+  List data;
+  String result="";
+
+  Future<String> getUsers() async {
+
+    var response = await http.get(
+      //encode the url if there is any sapce
+
+      Uri.encodeFull(api),
+      // only accept headers
+
+      headers: {"Accept":"application/json" }
+    );
+
+    print(response.body);
+
+    setState(() {
+      
+      var convertToJson = jsonDecode(response.body);
+      data = convertToJson["results"];
+      result = response.body;
+
+          });
+
+          return "SUCCESS";
+  }
+  @override
+  void initState() {
+    super.initState();
+    getUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(appBar: CustomWidget.getAppbarWithBack(context, Constants.TITLE_API_CALLING),
+    body: Container(
+      child: Center(
+        child: result.isNotEmpty ? ListView.builder(
+          itemCount: data==null ?  0 : data.length ,
+          itemBuilder: (BuildContext context ,int index){
+         
+            return Container(
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                     Card(child: Container(
+                       padding: EdgeInsets.all(10.0),
+                       child:  Text(data[index]['name']),
+                     ),)
+                    
+                  ],
+                ),
+              ),
+            );
+          })
+          
+          : CircularProgressIndicator(),
+      ),
+
+    ),),
+    );
+  }
+}
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,9 +1,9 @@
 import 'dart:math' as Math;
 import 'dart:ui';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import 'package:flutter/material.dart';
 
-const _TAG = 'Util';
 
 class StringUtil {
   static bool equalIgnoreCase(String s1, String s2) =>
@@ -65,4 +65,18 @@ class CustomWidget {
         leading: IconButton(icon: Icon(Icons.arrow_back),onPressed:() =>Navigator.of(context).pop(),),
 
       );
+
+  static showToast(String title){
+    Fluttertoast.showToast(
+                            msg: title,
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.BOTTOM,
+                            timeInSecForIos: 1,
+                            backgroundColor: Colors.red,
+                            textColor: Colors.white,
+                            fontSize: 16.0);
+  }
 }
+
+
+
